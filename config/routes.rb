b391e6eeee6411d50a39f5e8f56b29976ac9ed46
sakamoto-waka <wakanmhl@gmail.com>
2022-04-29 23:08:43 +0000

    [update] config.hosts.clear

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'rooms/show'
   devise_for :users
+  get 'rooms/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
